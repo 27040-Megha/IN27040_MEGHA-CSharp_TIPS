79c7763c8816b30514b9748637c713d6d9decced
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-01 18:23:31 +0530

    refactor: refactor code

--- src/Assignment10/CalculatorApp/ApplicationLayer/Service/CalculatorService.cs
+++ src/Assignment10/CalculatorApp/ApplicationLayer/Service/CalculatorService.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using CalculatorApp.ApplicationLayer.Service;
 using CalculatorApp.Domain;
 
 namespace CalculatorApp.ApplicationLayer.Service
@@ -49,7 +45,7 @@ namespace CalculatorApp.ApplicationLayer.Service
 
         private List<string> SplitExpression(string inputExpression)
         {
-            string spacedExpression = "";
+            string spacedExpression = string.Empty;
             char[] op = { '+', '-', '*', '/' };
             foreach (char ch in inputExpression)
             {
--- src/Assignment10/CalculatorApp/PresentationLayer/Helper/InputValidation.cs
+++ src/Assignment10/CalculatorApp/PresentationLayer/Helper/InputValidation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculatorApp.PresentationLayer.Helper
+﻿namespace CalculatorApp.PresentationLayer.Helper
 {
     /// <summary>
     /// Input Validator class
