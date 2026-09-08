132005cee3002d21d7e639c448995ba3ec351c91
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-08 17:39:31 +0530

    feat: Make loop finite

---
+++ src/Assignment12/MemoryOptimizationWithFiniteLoop/MemoryEater.cs
@@ -0,0 +1,24 @@
+﻿namespace MemoryOptimization
+{
+    /// <summary>
+    /// MemoryEater class that contains method to allocate memory
+    /// </summary>
+    public class MemoryEater
+    {
+        private List<int[]> _memAlloc = new List<int[]>();
+
+        /// <summary>
+        /// Method that allocates memory in an infinite loop, also ensures that OutOfMemoryException doesn't occurs
+        /// </summary>
+        /// <param name="maxValue">Max Number of Loop Iterations</param>
+        public void Allocate(int maxValue)
+        {
+            int i = 1;
+            while (i++ <= maxValue)
+            {
+                this._memAlloc.Add(new int[1000]);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
