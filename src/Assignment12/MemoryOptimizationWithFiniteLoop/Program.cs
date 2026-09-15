88d3e258e2cb3e43d1f6544da127a7fa873abb10
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-15 14:58:14 +0530

    Assignment-12: Memory Optimization (#10)
    * feat: Complete optimized memory usage code
    
    * feat: Make loop finite
    
    * fix: Allocate method
    
    * docs: Add README
    
    * docs: Fix Documentation
    
    * fix: Resolve PR comments
    
    * docs: Add Observations in README

---
+++ src/Assignment12/MemoryOptimizationWithFiniteLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using MemoryOptimization;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Main Class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point of Application
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        public static void Main(string[] args)
+        {
+            MemoryEater me = new MemoryEater();
+            me.Allocate(1000);
+            me.CleanUp();
+            GC.Collect();
+        }
+    }
+}
