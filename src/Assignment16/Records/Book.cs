e4b5ba653e42fc6ebf513ffd7d67d9af14f55fe6
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-16 16:38:56 +0530

    feat: Complete Task-6

---
+++ src/Assignment16/Records/Book.cs
@@ -0,0 +1,42 @@
+﻿namespace Records
+{
+    public record Book
+    {
+        /// <summary>
+        /// Gets or Initializes Book Title
+        /// </summary>
+        /// <value>
+        /// Book Title
+        /// </value>
+        public string Title { get; init; }
+
+        /// <summary>
+        /// Gets or Initializes Author of Book
+        /// </summary>
+        /// <value>
+        /// Book Author
+        /// </value>
+        public string Author { get; init; }
+
+        /// <summary>
+        /// Gets or Initializes Book ISBN
+        /// </summary>
+        /// <value>
+        /// ISBN of Book
+        /// </value>
+        public string ISBN { get; init; }
+
+        /// <summary>
+        /// Deconstructs a book record with its properties
+        /// </summary>
+        /// <param name="title">Book Title</param>
+        /// <param name="author">Author</param>
+        /// <param name="isbn">ISBN</param>
+        public void Deconstruct(out string title, out string author, out string isbn)
+        {
+            title = this.Title;
+            author = this.Author;
+            isbn = this.ISBN;
+        }
+    }
+}
---
+++ src/Assignment16/Records/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Records;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var firstBook = new Book()
+            {
+                Title = "Jungle Book",
+                Author = "Rudyard Kipling",
+                ISBN = "1234-1234-1234",
+            };
+
+            var secondBook = new Book()
+            {
+                Title = "Harry Potter",
+                Author = "J.K. Rowling",
+                ISBN = "5678-5678-5678",
+            };
+
+            var thirdBook = new Book()
+            {
+                Title = "Jungle Book",
+                Author = "Rudyard Kipling",
+                ISBN = "1234-1234-1234",
+            };
+            DisplayBook(firstBook);
+            DisplayBook(secondBook);
+            DisplayBook(thirdBook);
+            DisplayValueEquality(firstBook, secondBook, thirdBook);
+            UpdateBookTitle(firstBook);
+            var newBook = CreateNewBook(firstBook);
+            Console.WriteLine(DisplayResource.NewBook);
+            DisplayBook(newBook);
+            DisplayBook(firstBook);
+        }
+
+        private static void DisplayValueEquality(Book firstBook, Book secondBook, Book thirdBook)
+        {
+            Console.WriteLine(string.Format(DisplayResource.CompareBook1With2, CheckValueEquality(firstBook, secondBook)));
+            Console.WriteLine(string.Format(DisplayResource.CompareBook1With3, CheckValueEquality(firstBook, thirdBook)));
+        }
+
+        private static void DisplayBook(Book book)
+        {
+            var (title, author, isbn) = book;
+            Console.WriteLine(string.Format(DisplayResource.DisplayBook, title, author, isbn));
+        }
+
+        private static bool CheckValueEquality(Book firstBook, Book secondBook)
+        {
+            return firstBook == secondBook;
+        }
+
+        private static void UpdateBookTitle(Book book)
+        {
+            // Records are primarily intended to support immutable data models
+            // In Book Record, the properties are defined as init-only property
+            // Properties can be set only during object creation and can't be modified later - Throws Compile-Time error when trying to change a property of Book record
+            // CS8852: Init-only property or indexer 'property' can only be assigned in an object initializer, or on 'this' or 'base' in an instance constructor or an 'init' accessor.
+            // book.Title = "The Jungle Book";
+        }
+
+        private static Book CreateNewBook(Book firstBook)
+        {
+            return firstBook with { Title = "THE JUNGLE BOOK" };
+        }
+    }
+}
