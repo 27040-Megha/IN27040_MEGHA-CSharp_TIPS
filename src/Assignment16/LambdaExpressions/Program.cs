e2896f6dc9579b5d709cdd8a9fe6e4b1583933b1
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-16 11:24:55 +0530

    feat: Complete basic implementation

---
+++ src/Assignment16/LambdaExpressions/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignments
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var listOfIntgers = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            var evenNumbers = listOfIntgers.Where((int number) => number % 2 == 0).ToList();
+
+        }
+
+        private static void DisplayList(List<int> list)
+        {
+            foreach (var number in list)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
