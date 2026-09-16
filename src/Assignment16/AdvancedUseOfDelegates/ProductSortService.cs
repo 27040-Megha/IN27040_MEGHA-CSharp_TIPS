75aba360246a55189f7e147b11bdabc2cbe78282
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-16 14:55:37 +0530

    feat: Complete Task-5

---
+++ src/Assignment16/AdvancedUseOfDelegates/ProductSortService.cs
@@ -0,0 +1,22 @@
+﻿namespace AdvancedUseOfDelegates
+{
+    public delegate int SortDelegate(Product firstProduct, Product secondProduct);
+
+    public class ProductSortService
+    {
+        public static int SortByName(Product firstProduct, Product secondProduct)
+        {
+            return firstProduct.ProductName.CompareTo(secondProduct.ProductName);
+        }
+
+        public static int SortByCategory(Product firstProduct, Product secondProduct)
+        {
+            return firstProduct.Category.CompareTo(secondProduct.Category);
+        }
+
+        public static int SortByPrice(Product firstProduct, Product secondProduct)
+        {
+            return firstProduct.Price.CompareTo(secondProduct.Price);
+        }
+    }
+}
