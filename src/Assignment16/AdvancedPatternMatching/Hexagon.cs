bf7cffce5e98e01f18eceb5adea9c5d9624572a2
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-16 19:46:23 +0530

    feat: Complete Task-7

---
+++ src/Assignment16/AdvancedPatternMatching/Hexagon.cs
@@ -0,0 +1,25 @@
+﻿namespace AdvancedPatternMatching
+{
+    /// <summary>
+    /// Shape Class that is not a subclass of Shape
+    /// </summary>
+    public class Hexagon
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hexagon"/> class.
+        /// </summary>
+        /// <param name="color">Color of the Shape</param>
+        public Hexagon(string color)
+        {
+            this.Color = color;
+        }
+
+        /// <summary>
+        /// Gets or Sets the value of color
+        /// </summary>
+        /// <value>
+        /// Color
+        /// </value>
+        public string Color { get; set; }
+    }
+}
---
+++ src/Assignment16/AdvancedPatternMatching/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿namespace AdvancedPatternMatching
+{
+    /// <summary>
+    /// Subclass inherits from Shape
+    /// </summary>
+    public class Rectangle : Shape
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rectangle"/> class.
+        /// </summary>
+        /// <param name="length">Length of rectangle</param>
+        /// <param name="width">Width of rectangle</param>
+        /// <param name="color">Color of Rectangle</param>
+        public Rectangle(double length, double width, string color)
+            : base(color)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        /// <summary>
+        /// Gets or Sets the value of Length of Rectangle
+        /// </summary>
+        /// <value>
+        /// Length of rectangle
+        /// </value>
+        public double Length { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the value of Width of Rectangle
+        /// </summary>
+        /// <value>
+        /// Width of rectangle
+        /// </value>
+        public double Width { get; set; }
+
+        /// <summary>
+        /// Calculates the Area
+        /// </summary>
+        /// <returns>Area</returns>
+        public override double CalculateArea()
+        {
+            this.Area = this.Length * this.Width;
+            return this.Area;
+        }
+    }
+}
