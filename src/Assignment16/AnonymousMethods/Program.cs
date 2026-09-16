7122d6fb848be631e0b8d2aa72278f04c54da29f
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-16 10:56:17 +0530

    feat: Complete Task-3

---
+++ src/Assignment16/AnonymousMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using AnonymousMethods;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] array = { 12, 3, 56, 14, 7, 82, 18, 2, 31 };
+            Array.Sort(array, delegate(int a, int b)
+            {
+                return b.CompareTo(a);
+            });
+
+            Console.WriteLine(DisplayResource.DescendingOrder);
+            foreach (var number in array)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
