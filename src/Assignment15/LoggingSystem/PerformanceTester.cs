44c99c774e66e7b751e94bbce4fdb101dd920dd2
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-19 20:33:51 +0530

    feat: Complete Task4

---
+++ src/Assignment15/LoggingSystem/PerformanceTester.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace LoggingSystem
+{
+    /// <summary>
+    /// Contains method to create multiple tasks and call Logger methods
+    /// </summary>
+    public class PerformanceTester
+    {
+        /// <summary>
+        /// Create 100 tasks (Simulating 100 users logging errors at the same time)
+        /// Each task runs Logger.LogError() which logs errors to the same file
+        /// </summary>
+        /// <returns>Task object - Asynchronous Operation result</returns>
+        public static async Task RunLoadForSameFileLogger()
+        {
+            var tasks = new Task[100];
+            for (int i = 0; i < 100; i++)
+            {
+                string currentUserId = i.ToString();
+                tasks[i] = Task.Run(() => Logger.LogError($"ERROR:USERID{currentUserId} Don't have access! "));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// Create 100 tasks (Simulating 100 users logging errors at the same time)
+        /// Each task runs IndependentLogger.LogErrorAsync() which logs errors to independent files
+        /// </summary>
+        /// <returns>Task object - Asynchronous Operation result</returns>
+        public static async Task RunLoadForIndependentLogger()
+        {
+            var tasks = new Task[100];
+            for (int i = 0; i < 100; i++)
+            {
+                string currentUserId = i.ToString();
+                tasks[i] = Task.Run(async () => await IndependentLogger.LogErrorAsync(currentUserId, $"ERROR:USERID{currentUserId} Don't have access! "));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
