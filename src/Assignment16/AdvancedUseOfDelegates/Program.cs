53879779a67d4316f41cc40c5625e492dcc9c0fd
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-21 16:18:38 +0530

    fix: Fix naming conventions

--- src/Assignment16/AdvancedUseOfDelegates/Program.cs
+++ src/Assignment16/AdvancedUseOfDelegates/Program.cs
@@ -12,13 +12,13 @@ namespace Assignments
         private static void Main(string[] args)
         {
             var products = LoadProducts();
-            SortDelegate sortDelegates = ProductSortService.SortByName;
+            SortDelegate sortDelegates = ProductSortingService.SortByName;
             TextColor.WriteColorLine(DisplayResource.SortByName, ConsoleColor.Cyan);
             SortAndDisplay(sortDelegates, products);
-            sortDelegates = ProductSortService.SortByCategory;
+            sortDelegates = ProductSortingService.SortByCategory;
             TextColor.WriteColorLine(DisplayResource.SortByCategory, ConsoleColor.Cyan);
             SortAndDisplay(sortDelegates, products);
-            sortDelegates = ProductSortService.SortByPrice;
+            sortDelegates = ProductSortingService.SortByPrice;
             TextColor.WriteColorLine(DisplayResource.SortByPrice, ConsoleColor.Cyan);
             SortAndDisplay(sortDelegates, products);
         }
@@ -26,14 +26,14 @@ namespace Assignments
         private static List<Product> LoadProducts()
         {
             return new List<Product>()
-                   {
-                    new Product("Pen", "Stationary", 50),
-                    new Product("Notebook", "Stationary", 120),
-                    new Product("Wireless Mouse", "Electronics", 900),
-                    new Product("Bluetooth Earbuds", "Electronics", 2500),
-                    new Product("Bread Toaster", "Kitchen", 1600),
-                    new Product("Sticky Notes", "Stationary", 60),
-                   };
+            {
+                new Product("Pen", "Stationary", 50),
+                new Product("Notebook", "Stationary", 120),
+                new Product("Wireless Mouse", "Electronics", 900),
+                new Product("Bluetooth Earbuds", "Electronics", 2500),
+                new Product("Bread Toaster", "Kitchen", 1600),
+                new Product("Sticky Notes", "Stationary", 60),
+            };
         }
 
         private static void SortAndDisplay(SortDelegate sortDelegate, List<Product> products)
