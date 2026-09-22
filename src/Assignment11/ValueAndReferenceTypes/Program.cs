9f7282f982ad219f2cabc4a7644ba11ececa02bc
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-22 15:24:43 +0530

    Assignment-11: Memory Management
    * feat: Complete initial implementation of Task1,2
    
    * feat: Add Task3
    
    * feat: Complete Task3
    
    * feat: Display memory usage in console
    
    * feat: Complete Assignment11
    
    * docs: Add Documenation link in README
    
    * refactor: Refactor Update Service code
    
    * fix: Resolve peer review comments
    
    * fix: Resolve PR comments

---
+++ src/Assignment11/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ValueAndReferenceTypes.ApplicationLayer.Service;
+using ValueAndReferenceTypes.PresentationLayer.View;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Entry point of Application
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+            var updateService = new UpdateService();
+            var consoleOperator = new ConsoleOperations(updateService);
+            consoleOperator.Run();
+        }
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                Console.WriteLine($"Exception Message: {ex.Message}");
+            }
+        }
+    }
+}
