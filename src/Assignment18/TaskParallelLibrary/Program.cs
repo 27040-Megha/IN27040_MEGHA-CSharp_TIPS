f6972950bc6583f8d815e9c907eafe7f1862076b
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-24 15:55:07 +0530

    feat: Complete Task2

---
+++ src/Assignment18/TaskParallelLibrary/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Declares an array of integers and compares the difference between time taken for execution between Calculating Square of array elements Sequentially vs Parallely
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        public static void Main(string[] args)
+        {
+            int[] array = Enumerable.Range(1, 10000).ToArray();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            CalculateSquareParallelly(array);
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken to calculate square of array elements Parallely: {stopwatch.ElapsedMilliseconds} ms");
+            array = Enumerable.Range(1, 10000).ToArray();
+            stopwatch.Restart();
+            CalculateSquareSequentially(array);
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken to calculate square of array elements Sequentially: {stopwatch.ElapsedMilliseconds} ms");
+            DisplayArray(array);
+        }
+
+        private static void DisplayArray(int[] array)
+        {
+            Console.WriteLine("Square of Array Numbers: ");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void CalculateSquareParallelly(int[] array)
+        {
+            Parallel.For(0, array.Length, index =>
+            {
+                array[index] = array[index] * array[index];
+            });
+        }
+
+        private static void CalculateSquareSequentially(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] * array[i];
+            }
+        }
+    }
+}
---
+++ src/Assignment18/TaskParallelLibrary/README.md
@@ -0,0 +1,61 @@
+﻿# Assignment-18 : Async/Await, Task Parallel Library, and Multi-Threading in C#
+ 
+##  Task 2: Implementing and Understanding Task Parallel Library
+
+### Concept
+
+### Task Parallel Library
+
+- Collection of APIs for parallel and asynchronous programming.
+- Namespace: System.Threading.Tasks
+- Task class: Unit of work that can be executed asynchronously on a separate thread.
+---
+
+### Parallel.ForEach
+
+- Used to execute loop parallely.
+- Useful for processing large collections, where each iteration of loop will be executed independently.
+- Optimizes the use of available CPU cores.
+
+### Where to use Parallel.For vs Parallel.ForEach
+
+Parallel.For:
+
+- Input: Index Range.
+- Best for Arrays, Structures or fixed numeric sequence loops.
+- Index Access: Built-in.
+
+Parallel.ForEach:
+
+- Input: Any collection implementing IEnumerable&lt;T&gt;
+- Best for Lists, Collections and enumerables where index is not strictly required.
+- Index Access: Explictly should be given.
+
+---
+## Implementation
+
+### Program.cs
+
+CalculateSquareSequentially()
+
+- Calculate squares of array elements sequentially using a for loop.
+
+CalculateSquareParallelly()
+
+- Calculate squares of array elements using Parrallelly using Parallel.For/Parallel.ForEach.
+
+DisplayArray()
+
+- Display the squares of array elements after calculating their squares.
+
+Main()
+
+Time Taken to process large Data from 1 to 1000000000:
+
+Using Parallel.ForEach: 32844 ms 
+Using Parallel.For: 3396 ms
+Sequentially: 5438 ms
+
+Best Option for this Task to calculate square of array elements parallely: Parallel.For
+
+---
