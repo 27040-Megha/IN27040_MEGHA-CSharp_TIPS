af428ddde46497d3dbffedcfcc2e10dcd107f7eb
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-09-24 16:51:05 +0530

    feat: Complete Task-3

---
+++ src/Assignment18/Multithreading/Program.cs
@@ -0,0 +1,59 @@
+﻿using Multithreading;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Creates 3 threads to perform 3 operations concurrently, and wait using Thread.join() and then prints result to the console
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        public static void Main(string[] args)
+        {
+            int[] array = Enumerable.Range(1, 100).ToArray();
+            int sum = 0, average = 0;
+            var thread1 = new Thread(() => sum = CalculateSum(array));
+            var thread2 = new Thread(() => SortArray(array));
+            var thread3 = new Thread(() => average = FindAverage(array));
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+            DisplayResult(sum, average, array);
+        }
+
+        private static void DisplayResult(int sum, int average, int[] array)
+        {
+            Console.WriteLine(string.Format(DisplayResource.Result, sum, average));
+            DisplayArray(array);
+        }
+
+        private static int CalculateSum(int[] array)
+        {
+            return array.Sum();
+        }
+
+        private static void SortArray(int[] array)
+        {
+            Array.Sort(array);
+        }
+
+        private static void DisplayArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int FindAverage(int[] array)
+        {
+            return array.Sum() / array.Length;
+        }
+    }
+}
---
+++ src/Assignment18/Multithreading/README.md
@@ -0,0 +1,34 @@
+﻿# Assignment-18 : Async/Await, Task Parallel Library, and Multi-Threading in C#
+ 
+## Task 3: Advanced Understanding and Usage of Multi-Threading
+
+### Concept
+
+Multithreading
+
+- Thread: Execution unit within a process.
+- Process needs a thread to run.
+- Multithreading: Runs multiple threads concurrently or parallely.
+- Each thread has its own call stack, and share managed heap.
+- Thread.Join() : Waits for the respective thread to complete its execution, otherwise the main thread will complete its execution and exits.
+- Difference between Task and Thread: Threads does not return anything, While Tasks can return data.
+___
+
+## Implementation
+
+### Program.cs
+
+Methods
+
+- int CalculateSum(int[] array) - Returns the sum of array elements.
+- void SortArray(int[] array) - Sort the given array elements.
+- int FindAverage(int[] array) - Returns the average of array elements.
+- DisplayResult(int sum, int average, int[] array) - Displays sum, average and calls DisplayArray.
+- void DisplayArray(int[] array) - Displays array elements.
+
+Main()
+
+- Creates 3 threads.
+- Each thread will run parallelly and executes CalculateSum, SortArray and FindAverage.
+- Using Thread.Join(), wait for threads to complete its execution.
+- Print Result using DisplayResult
